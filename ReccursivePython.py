209781ff5f6e0ba58f0aefa2b4db861816bcbdf1
Yu Han Tey <112633796+yuhantey7@users.noreply.github.com>
2023-11-15 14:41:30 +0800

    Update ReccursivePython.py

--- ReccursivePython.py
+++ ReccursivePython.py
@@ -4,3 +4,18 @@ def fun(n, res=1):
     return fun(n - 1, n * res)
 
 print(fun(5))
+
+
+
+# def fun(n, res=1):
+#  (5,1)
+#  (4,20)
+#  (3,60)
+#  (2,120)
+#  (1,120)
+#     if n <= 0: 
+#         return res 
+#     return fun(n - 1, n * res) 
+
+
+# print(fun(5))
